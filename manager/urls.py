2b20fc50253cc8f4dcabac721aed568e1627e896
iamgope <amangope005@gmail.com>
2021-11-30 21:53:49 +0530

    added

--- manager/urls.py
+++ manager/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('auth.urls')),
@@ -26,5 +29,5 @@ urlpatterns = [
 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
