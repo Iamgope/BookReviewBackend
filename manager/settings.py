a9edbc6c7ed735eb598fab9f8765919d6fdff4ab
iamgope <amangope005@gmail.com>
2021-11-30 09:32:31 +0530

    make it better

--- manager/settings.py
+++ manager/settings.py
@@ -34,18 +34,16 @@ ALLOWED_HOSTS = ['*',]
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic',
-    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
+
     'corsheaders',
     'rest_framework',
-    'Base'
-
-
+    'Base',
 
 ]
 
